7e022c53647c2e7c93a599db0b3729c8b7b25185
Charles Okeke <okekecharles91@gmail.com>
2024-06-24 16:41:25 +0100

    customized admin title and header
    Signed-off-by: Charles Okeke <okekecharles91@gmail.com>

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 from .models import (
     Contact, EmployeeProfile, 
     CustomerProfile, CustomUser, Comment)
 from ecommerce.admin import OrderInline, ItemInline
+
+
+#headers and titles
+admin.site.site_header = "Ecommerce App"
+admin.site.site_title = "...one stop shop on the web."
 
 #inlines
 class CustomerInline(admin.TabularInline):
